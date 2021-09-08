a230a288807f775710339dc1da00e094efa3d20d
Poresh2222 <st.yahor.poresh@gmail.com>
2021-09-08 12:00:07 +0200

    5.3.7 Erro

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,9 @@
+use sqlx::postgres::PgPoolOptions;
+use std::net::TcpListener;
+
 use zero2prod::configuration::get_configuration;
 use zero2prod::startup::run;
 use zero2prod::telemetry::{get_subscriber, init_subscriber};
-use sqlx::postgres::PgPool;
-use std::net::TcpListener;
 
 
 #[actix_web::main]
@@ -12,14 +13,19 @@ async fn main() -> std::io::Result<()> {
     init_subscriber(subscriber);
     
     let configuration = get_configuration().expect("Failed to read configuration.");
-    let connection = PgPool::connect(&configuration.database.connection_string())
+    let connection_pool = PgPoolOptions::new()
+        .connect_timeout(std::time::Duration::from_secs(2))
+        .connect_with(configuration.database.with_db())
         .await
         .expect("Failed to connect to Postgres.");
 
-    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let address = format!("{}:{}", 
+        configuration.application.host, configuration.application.port
+    );
+
     let listener = TcpListener::bind(address)?;
     
-    run(listener, connection)?.await?;
+    run(listener, connection_pool)?.await?;
 
     Ok(())
 
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,10 +1,10 @@
-use zero2prod::configuration::{get_configuration, DatabaseSettings};
-use zero2prod::startup::run;
-use zero2prod::telemetry::{get_subscriber, init_subscriber};
-
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use std::net::TcpListener;
 use uuid::Uuid;
+
+use zero2prod::configuration::{get_configuration, DatabaseSettings};
+use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
 
 lazy_static::lazy_static! {
