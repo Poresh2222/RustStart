0f73f98d27a1b3a00ad076235a4d7bdd99f8988c
Poresh2222 <st.yahor.poresh@gmail.com>
2021-09-13 16:29:08 +0200

    6.10

--- src/lib.rs
+++ src/lib.rs
@@ -1,36 +1,6 @@
-#![allow(clippy::toplevel_ref_arg)]
-
+//#![allow(clippy::toplevel_ref_arg)]
 pub mod configuration;
+pub mod domain;
 pub mod routes;
 pub mod startup;
 pub mod telemetry;
-
-use actix_web::{web, App, HttpResponse, HttpServer};
-use actix_web::dev::Server;
-use std::net::TcpListener;
-
-pub fn run(listner: TcpListener) -> Result<Server, std::io::Error> {
-    let server = HttpServer::new(|| {
-        App::new()
-            .route("/health_check", web::get().to(health_check))
-            .route("/subscriptions", web::post().to(subscribe))
-    })
-    .listen(listner)?
-    .run();
-    
-    Ok(server)
-}
-
-#[derive(serde::Deserialize)]
-struct FormData {
-    email: String,
-    name: String
-}
-
-async fn health_check() -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
-
-async fn subscribe(_form: web::Form<FormData>) -> HttpResponse {
-    HttpResponse::Ok().finish()
-}
--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -2,6 +2,9 @@ use actix_web::{web, HttpResponse};
 use chrono::Utc;
 use sqlx::PgPool;
 use uuid::Uuid;
+use unicode_segmentation::UnicodeSegmentation;
+
+use crate::domain::{NewSubscriber, SubscriberName};
 
 #[derive(serde::Deserialize)]
 pub struct FormData {
@@ -23,9 +26,17 @@ pub async fn subscribe(
     form: web::Form<FormData>, 
     pool: web::Data<PgPool>
 ) -> Result<HttpResponse, HttpResponse> {
-    insert_subscriber(&pool, &form)
+
+    let new_subscriber = NewSubscriber {
+
+        email: form.0.email,
+        name: SubscriberName::parse(form.0.name),
+
+    };
+
+    insert_subscriber(&pool, &new_subscriber)
         .await
-        .map_err(|_| HttpResponse::InternalServerError().finish())?;
+        .map_err(|_|HttpResponse::InternalServerError().finish())?;
 
     Ok(HttpResponse::Ok().finish())
 
@@ -33,11 +44,11 @@ pub async fn subscribe(
 
 #[tracing::instrument(
     name = "Saving new subscriber details in the database",
-    skip(form, pool)
+    skip(new_subscriber, pool)
 )]
 pub async fn insert_subscriber(
     pool: &PgPool,
-    form: &FormData,
+    new_subscriber: &NewSubscriber,
 ) -> Result<(), sqlx::Error> {
     sqlx::query!(
         r#"
@@ -45,8 +56,8 @@ pub async fn insert_subscriber(
     VALUES ($1, $2, $3, $4)
             "#,
         Uuid::new_v4(),
-        form.email,
-        form.name,
+        new_subscriber.email,
+        new_subscriber.name.as_ref(),
         Utc::now()
     )
     .execute(pool)
@@ -57,5 +68,19 @@ pub async fn insert_subscriber(
     })?;
 
     Ok(())
+
+}
+
+pub fn is_valid_name(s: &str) -> bool {
+
+    let is_empty_or_whitespace = s.trim().is_empty();
+
+    let is_too_long = s.graphemes(true).count() > 256;
+
+    let forbidden_characters = ['/', '(', ')', '"', '<', '>', '\\', '{', '}'];
+
+    let contains_forbidden_characters = s.chars().any(|g| forbidden_characters.contains(&g));
+
+    !(is_empty_or_whitespace || is_too_long || contains_forbidden_characters)
 
 }
